7522b260c315907be8ca5fcc777f0250c312541d
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2024-10-03 12:11:53 +0100

    update

--- model_predictive_speed_and_steer_control.py
+++ model_predictive_speed_and_steer_control.py
@@ -182,10 +182,10 @@ def get_nparray_from_matrix(x):
     return np.array(x).flatten()
 
 
-def calc_nearest_index(state, cx, cy, cyaw, pind):
+def calc_nearest_index(state, course_xs, course_ys, course_yaws, pind):
 
-    dx = [state.x - icx for icx in cx[pind : (pind + N_IND_SEARCH)]]
-    dy = [state.y - icy for icy in cy[pind : (pind + N_IND_SEARCH)]]
+    dx = [state.x - icx for icx in course_xs[pind : (pind + N_IND_SEARCH)]]
+    dy = [state.y - icy for icy in course_ys[pind : (pind + N_IND_SEARCH)]]
 
     d = [idx**2 + idy**2 for (idx, idy) in zip(dx, dy)]
 
@@ -195,10 +195,10 @@ def calc_nearest_index(state, cx, cy, cyaw, pind):
 
     mind = math.sqrt(mind)
 
-    dxl = cx[ind] - state.x
-    dyl = cy[ind] - state.y
+    dxl = course_xs[ind] - state.x
+    dyl = course_ys[ind] - state.y
 
-    angle = pi_2_pi(cyaw[ind] - math.atan2(dyl, dxl))
+    angle = pi_2_pi(course_yaws[ind] - math.atan2(dyl, dxl))
     if angle < 0:
         mind *= -1
 
@@ -231,7 +231,7 @@ def iterative_linear_mpc_control(xref, x0, dref, oa, od):
         oa = [0.0] * T
         od = [0.0] * T
 
-    for i in range(MAX_ITER):
+    for _ in range(MAX_ITER):
         xbar = predict_motion(x0, oa, od, xref)
         poa, pod = oa[:], od[:]
         oa, od, ox, oy, oyaw, ov = linear_mpc_control(xref, xbar, x0, dref)
@@ -275,11 +275,11 @@ def linear_mpc_control(xref, xbar, x0, dref):
 
     cost += cvxpy.quad_form(xref[:, T] - x[:, T], Qf)
 
-    constraints += [x[:, 0] == x0]
-    constraints += [x[2, :] <= MAX_SPEED]
-    constraints += [x[2, :] >= MIN_SPEED]
-    constraints += [cvxpy.abs(u[0, :]) <= MAX_ACCEL]
-    constraints += [cvxpy.abs(u[1, :]) <= MAX_STEER]
+    constraints.append(x[:, 0] == x0)
+    constraints.append(x[2, :] <= MAX_SPEED)
+    constraints.append(x[2, :] >= MIN_SPEED)
+    constraints.append(cvxpy.abs(u[0, :]) <= MAX_ACCEL)
+    constraints.append(cvxpy.abs(u[1, :]) <= MAX_STEER)
 
     prob = cvxpy.Problem(cvxpy.Minimize(cost), constraints)
     prob.solve(solver=cvxpy.CLARABEL, verbose=False)
@@ -299,39 +299,39 @@ def linear_mpc_control(xref, xbar, x0, dref):
     return oa, odelta, ox, oy, oyaw, ov
 
 
-def calc_ref_trajectory(state, cx, cy, cyaw, ck, sp, dl, pind):
+def calc_ref_trajectory(state, course_xs, course_ys, course_yaws, course_curvatures, speed_profile, course_tick, pind):
     xref = np.zeros((NX, T + 1))
     dref = np.zeros((1, T + 1))
-    ncourse = len(cx)
+    ncourse = len(course_xs)
 
-    ind, _ = calc_nearest_index(state, cx, cy, cyaw, pind)
+    ind, _ = calc_nearest_index(state, course_xs, course_ys, course_yaws, pind)
 
     if pind >= ind:
         ind = pind
 
-    xref[0, 0] = cx[ind]
-    xref[1, 0] = cy[ind]
-    xref[2, 0] = sp[ind]
-    xref[3, 0] = cyaw[ind]
+    xref[0, 0] = course_xs[ind]
+    xref[1, 0] = course_ys[ind]
+    xref[2, 0] = speed_profile[ind]
+    xref[3, 0] = course_yaws[ind]
     dref[0, 0] = 0.0  # steer operational point should be 0
 
     travel = 0.0
 
     for i in range(1, T + 1):
         travel += abs(state.v) * DT
-        dind = int(round(travel / dl))
+        dind = int(round(travel / course_tick))
 
         if (ind + dind) < ncourse:
-            xref[0, i] = cx[ind + dind]
-            xref[1, i] = cy[ind + dind]
-            xref[2, i] = sp[ind + dind]
-            xref[3, i] = cyaw[ind + dind]
+            xref[0, i] = course_xs[ind + dind]
+            xref[1, i] = course_ys[ind + dind]
+            xref[2, i] = speed_profile[ind + dind]
+            xref[3, i] = course_yaws[ind + dind]
             dref[0, i] = 0.0
         else:
-            xref[0, i] = cx[ncourse - 1]
-            xref[1, i] = cy[ncourse - 1]
-            xref[2, i] = sp[ncourse - 1]
-            xref[3, i] = cyaw[ncourse - 1]
+            xref[0, i] = course_xs[ncourse - 1]
+            xref[1, i] = course_ys[ncourse - 1]
+            xref[2, i] = speed_profile[ncourse - 1]
+            xref[3, i] = course_yaws[ncourse - 1]
             dref[0, i] = 0.0
 
     return xref, ind, dref
@@ -357,27 +357,27 @@ def check_goal(state, goal, tind, nind):
     return False
 
 
-def do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state):
+def do_simulation(course_xs, course_ys, course_yaws, course_curvatures, speed_profile, course_tick, initial_state):
     """
     Simulation
 
-    cx: course x position list
-    cy: course y position list
-    cy: course yaw position list
-    ck: course curvature list
-    sp: speed profile
-    dl: course tick [m]
+    course_xs: course x position list
+    course_ys: course y position list
+    course_yaws: course yaw position list
+    course_curvatures: course curvature list
+    speed_profile: speed profile
+    course_tick: course tick [m]
 
     """
 
-    goal = [cx[-1], cy[-1]]
+    goal = [course_xs[-1], course_ys[-1]]
 
     state = initial_state
 
     # initial yaw compensation
-    if state.yaw - cyaw[0] >= math.pi:
+    if state.yaw - course_yaws[0] >= math.pi:
         state.yaw -= math.pi * 2.0
-    elif state.yaw - cyaw[0] <= -math.pi:
+    elif state.yaw - course_yaws[0] <= -math.pi:
         state.yaw += math.pi * 2.0
 
     time = 0.0
@@ -388,14 +388,16 @@ def do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state):
     t = [0.0]
     d = [0.0]
     a = [0.0]
-    target_ind, _ = calc_nearest_index(state, cx, cy, cyaw, 0)
+    target_ind, _ = calc_nearest_index(state, course_xs, course_ys, course_yaws, 0)
 
     odelta, oa = None, None
 
-    cyaw = smooth_yaw(cyaw)
+    course_yaws = smooth_yaw(course_yaws)
 
     while MAX_TIME >= time:
-        xref, target_ind, dref = calc_ref_trajectory(state, cx, cy, cyaw, ck, sp, dl, target_ind)
+        xref, target_ind, dref = calc_ref_trajectory(
+            state, course_xs, course_ys, course_yaws, course_curvatures, speed_profile, course_tick, target_ind
+        )
 
         x0 = [state.x, state.y, state.v, state.yaw]  # current state
 
@@ -416,7 +418,7 @@ def do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state):
         d.append(di)
         a.append(ai)
 
-        if check_goal(state, goal, target_ind, len(cx)):
+        if check_goal(state, goal, target_ind, len(course_xs)):
             print("Goal")
             break
 
@@ -426,10 +428,10 @@ def do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state):
             plt.gcf().canvas.mpl_connect("key_release_event", lambda event: [exit(0) if event.key == "escape" else None])
             if ox is not None:
                 plt.plot(ox, oy, "xr", label="MPC")
-            plt.plot(cx, cy, "-r", label="course")
+            plt.plot(course_xs, course_ys, "-r", label="course")
             plt.plot(x, y, "ob", label="trajectory")
             plt.plot(xref[0, :], xref[1, :], "xk", label="xref")
-            plt.plot(cx[target_ind], cy[target_ind], "xg", label="target")
+            plt.plot(course_xs[target_ind], course_ys[target_ind], "xg", label="target")
             plot_car(state.x, state.y, state.yaw, steer=di)
             plt.axis("equal")
             plt.grid(True)
@@ -439,20 +441,20 @@ def do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state):
     return t, x, y, yaw, v, d, a
 
 
-def calc_speed_profile(cx, cy, cyaw, target_speed):
+def calc_speed_profile(course_xs, course_ys, course_yaws, target_speed):
 
-    speed_profile = [target_speed] * len(cx)
+    speed_profile = [target_speed] * len(course_xs)
     direction = 1.0  # forward
 
     # Set stop point
-    for i in range(len(cx) - 1):
-        dx = cx[i + 1] - cx[i]
-        dy = cy[i + 1] - cy[i]
+    for i in range(len(course_xs) - 1):
+        dx = course_xs[i + 1] - course_xs[i]
+        dy = course_ys[i + 1] - course_ys[i]
 
         move_direction = math.atan2(dy, dx)
 
         if dx != 0.0 and dy != 0.0:
-            dangle = abs(pi_2_pi(move_direction - cyaw[i]))
+            dangle = abs(pi_2_pi(move_direction - course_yaws[i]))
             if dangle >= math.pi / 4.0:
                 direction = -1.0
             else:
@@ -484,72 +486,72 @@ def smooth_yaw(yaw):
     return yaw
 
 
-def get_straight_course(dl):
+def get_straight_course(course_tick):
     ax = [0.0, 5.0, 10.0, 20.0, 30.0, 40.0, 50.0]
     ay = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=dl)
+    course_xs, course_ys, course_yaws, course_curvatures, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=course_tick)
 
-    return cx, cy, cyaw, ck
+    return course_xs, course_ys, course_yaws, course_curvatures
 
 
-def get_straight_course2(dl):
+def get_straight_course2(course_tick):
     ax = [0.0, -10.0, -20.0, -40.0, -50.0, -60.0, -70.0]
     ay = [0.0, -1.0, 1.0, 0.0, -1.0, 1.0, 0.0]
-    cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=dl)
+    course_xs, course_ys, course_yaws, course_curvatures, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=course_tick)
 
-    return cx, cy, cyaw, ck
+    return course_xs, course_ys, course_yaws, course_curvatures
 
 
-def get_straight_course3(dl):
+def get_straight_course3(course_tick):
     ax = [0.0, -10.0, -20.0, -40.0, -50.0, -60.0, -70.0]
     ay = [0.0, -1.0, 1.0, 0.0, -1.0, 1.0, 0.0]
-    cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=dl)
+    course_xs, course_ys, course_yaws, course_curvatures, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=course_tick)
 
-    cyaw = [i - math.pi for i in cyaw]
+    course_yaws = [i - math.pi for i in course_yaws]
 
-    return cx, cy, cyaw, ck
+    return course_xs, course_ys, course_yaws, course_curvatures
 
 
-def get_forward_course(dl):
+def get_forward_course(course_tick):
     ax = [0.0, 60.0, 125.0, 50.0, 75.0, 30.0, -10.0]
     ay = [0.0, 0.0, 50.0, 65.0, 30.0, 50.0, -20.0]
-    cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=dl)
+    course_xs, course_ys, course_yaws, course_curvatures, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=course_tick)
 
-    return cx, cy, cyaw, ck
+    return course_xs, course_ys, course_yaws, course_curvatures
 
 
-def get_switch_back_course(dl):
+def get_switch_back_course(course_tick):
     ax = [0.0, 30.0, 6.0, 20.0, 35.0]
     ay = [0.0, 0.0, 20.0, 35.0, 20.0]
-    cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=dl)
+    course_xs, course_ys, course_yaws, course_curvatures, s = cubic_spline_planner.calc_spline_course(ax, ay, ds=course_tick)
     ax = [35.0, 10.0, 0.0, 0.0]
     ay = [20.0, 30.0, 5.0, 0.0]
-    cx2, cy2, cyaw2, ck2, s2 = cubic_spline_planner.calc_spline_course(ax, ay, ds=dl)
+    cx2, cy2, cyaw2, ck2, s2 = cubic_spline_planner.calc_spline_course(ax, ay, ds=course_tick)
     cyaw2 = [i - math.pi for i in cyaw2]
-    cx.extend(cx2)
-    cy.extend(cy2)
-    cyaw.extend(cyaw2)
-    ck.extend(ck2)
+    course_xs.extend(cx2)
+    course_ys.extend(cy2)
+    course_yaws.extend(cyaw2)
+    course_curvatures.extend(ck2)
 
-    return cx, cy, cyaw, ck
+    return course_xs, course_ys, course_yaws, course_curvatures
 
 
 def main():
     print(__file__ + " start!!")
     start = time.time()
 
-    dl = 1.0  # course tick
-    # cx, cy, cyaw, ck = get_straight_course(dl)
-    # cx, cy, cyaw, ck = get_straight_course2(dl)
-    # cx, cy, cyaw, ck = get_straight_course3(dl)
-    # cx, cy, cyaw, ck = get_forward_course(dl)
-    cx, cy, cyaw, ck = get_switch_back_course(dl)
+    course_tick = 1.0  # course tick
+    # course_xs, course_ys, course_yaws, course_curvatures = get_straight_course(course_tick)
+    # course_xs, course_ys, course_yaws, course_curvatures = get_straight_course2(course_tick)
+    # course_xs, course_ys, course_yaws, course_curvatures = get_straight_course3(course_tick)
+    # course_xs, course_ys, course_yaws, course_curvatures = get_forward_course(course_tick)
+    course_xs, course_ys, course_yaws, course_curvatures = get_switch_back_course(course_tick)
 
-    sp = calc_speed_profile(cx, cy, cyaw, TARGET_SPEED)
+    speed_profile = calc_speed_profile(course_xs, course_ys, course_yaws, TARGET_SPEED)
 
-    initial_state = State(x=cx[0], y=cy[0], yaw=cyaw[0], v=0.0)
+    initial_state = State(x=course_xs[0], y=course_ys[0], yaw=course_yaws[0], v=0.0)
 
-    t, x, y, yaw, v, d, a = do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state)
+    t, x, y, yaw, v, d, a = do_simulation(course_xs, course_ys, course_yaws, course_curvatures, speed_profile, course_tick, initial_state)
 
     elapsed_time = time.time() - start
     print(f"calc time:{elapsed_time:.6f} [sec]")
@@ -557,7 +559,7 @@ def main():
     if show_animation:  # pragma: no cover
         plt.close("all")
         plt.subplots()
-        plt.plot(cx, cy, "-r", label="spline")
+        plt.plot(course_xs, course_ys, "-r", label="spline")
         plt.plot(x, y, "-g", label="tracking")
         plt.grid(True)
         plt.axis("equal")
@@ -578,14 +580,14 @@ def main2():
     print(__file__ + " start!!")
     start = time.time()
 
-    dl = 1.0  # course tick
-    cx, cy, cyaw, ck = get_straight_course3(dl)
+    course_tick = 1.0  # course tick
+    course_xs, course_ys, course_yaws, course_curvatures = get_straight_course3(course_tick)
 
-    sp = calc_speed_profile(cx, cy, cyaw, TARGET_SPEED)
+    speed_profile = calc_speed_profile(course_xs, course_ys, course_yaws, TARGET_SPEED)
 
-    initial_state = State(x=cx[0], y=cy[0], yaw=0.0, v=0.0)
+    initial_state = State(x=course_xs[0], y=course_ys[0], yaw=0.0, v=0.0)
 
-    t, x, y, yaw, v, d, a = do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state)
+    t, x, y, yaw, v, d, a = do_simulation(course_xs, course_ys, course_yaws, course_curvatures, speed_profile, course_tick, initial_state)
 
     elapsed_time = time.time() - start
     print(f"calc time:{elapsed_time:.6f} [sec]")
@@ -593,7 +595,7 @@ def main2():
     if show_animation:  # pragma: no cover
         plt.close("all")
         plt.subplots()
-        plt.plot(cx, cy, "-r", label="spline")
+        plt.plot(course_xs, course_ys, "-r", label="spline")
         plt.plot(x, y, "-g", label="tracking")
         plt.grid(True)
         plt.axis("equal")
