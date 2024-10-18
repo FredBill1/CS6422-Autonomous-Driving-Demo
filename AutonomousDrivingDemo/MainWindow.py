70caed8d868c39e4d8ca93c3725958c8b04f0dd3
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2024-10-18 16:14:57 +0100

    update

--- AutonomousDrivingDemo/MainWindow.py
+++ AutonomousDrivingDemo/MainWindow.py
@@ -15,8 +15,10 @@ from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QMainWindow
 
 from .CarSimulationNode import CarSimulationNode
+from .constants import MAP_HEIGHT, MAP_WIDTH
 from .GlobalPlannerNode import GlobalPlannerNode
 from .LocalPlannerNode import LocalPlannerNode
+from .MapServerNode import MapServerNode
 from .modeling.Car import Car
 from .modeling.Obstacles import Obstacles
 from .plotting.CarItem import CarItem
@@ -32,42 +34,6 @@ LOCAL_PLANNER_DELTA_TIME = 0.1
 LOCAL_PLANNER_UPDATE_INTERVAL = 0.2
 
 DASHBOARD_HISTORY_SIZE = 250
-
-MAP_WIDTH = 60.0
-MAP_HEIGHT = 60.0
-MAP_STEP = 1.0
-MAP_NUM_RANDOM_OBSTACLES = 40
-
-
-def _gen_known_obstacle_coordnates() -> npt.NDArray[np.floating[Any]]:
-    ox = [
-        np.arange(0, MAP_WIDTH, MAP_STEP),
-        np.full(np.ceil(MAP_HEIGHT / MAP_STEP).astype(int), MAP_WIDTH),
-        np.arange(0, MAP_WIDTH + MAP_STEP, MAP_STEP),
-        np.full(np.ceil(MAP_HEIGHT / MAP_STEP).astype(int) + 1, 0.0),
-        np.full(np.ceil(MAP_WIDTH / 3 * 2 / MAP_STEP).astype(int), MAP_WIDTH / 3),
-        np.full(np.ceil(MAP_HEIGHT / 3 * 2 / MAP_STEP).astype(int), 2 * MAP_WIDTH / 3),
-    ]
-    oy = [
-        np.full(np.ceil(MAP_WIDTH / MAP_STEP).astype(int), 0.0),
-        np.arange(0, MAP_HEIGHT, MAP_STEP),
-        np.full(np.ceil((MAP_WIDTH + MAP_STEP) / MAP_STEP).astype(int), MAP_HEIGHT),
-        np.arange(0, MAP_HEIGHT + MAP_STEP, MAP_STEP),
-        np.arange(0, MAP_WIDTH / 3 * 2, MAP_STEP),
-        MAP_HEIGHT - np.arange(0, MAP_HEIGHT / 3 * 2, MAP_STEP),
-    ]
-    return np.vstack((np.concatenate(ox), np.concatenate(oy))).T
-
-
-def _gen_unknown_obstacle_coordnates() -> npt.NDArray[np.floating[Any]]:
-    return np.random.uniform((0, 0), (MAP_WIDTH, MAP_HEIGHT), (MAP_NUM_RANDOM_OBSTACLES, 2))
-
-
-def _gen_random_car(obstacles: Obstacles) -> Car:
-    state = np.random.uniform((0, 0, -np.pi), (MAP_WIDTH, MAP_HEIGHT, np.pi))
-    while Car(*state).check_collision(obstacles):
-        state = np.random.uniform((0, 0, -np.pi), (MAP_WIDTH, MAP_HEIGHT, np.pi))
-    return Car(*state)
 
 
 class _CustomViewBox(pg.ViewBox):
@@ -90,11 +56,8 @@ class MainWindow(QMainWindow):
         super().__init__(*args, **kwargs)
 
         # prepare data
-        self._known_obstacle_coordinates = _gen_known_obstacle_coordnates()
-        self._unknown_obstacle_coordinates = _gen_unknown_obstacle_coordnates()
-        self._measured_state = _gen_random_car(
-            Obstacles(np.vstack((self._known_obstacle_coordinates, self._unknown_obstacle_coordinates)))
-        )
+        self._map_server_node = MapServerNode()
+        self._measured_state = self._map_server_node.generate_random_initial_state()
         self._measured_timestamp = 0.0
         self._measured_velocities: deque[float] = deque([0.0], maxlen=DASHBOARD_HISTORY_SIZE)
         self._measured_steers: deque[float] = deque([0.0], maxlen=DASHBOARD_HISTORY_SIZE)
@@ -132,10 +95,10 @@ class MainWindow(QMainWindow):
 
         # graphics items
         self._known_obstacles_item = pg.ScatterPlotItem(
-            *self._known_obstacle_coordinates.T, size=5, symbol="o", pen=None, brush=(255, 0, 0)
+            *self._map_server_node.known_obstacle_coordinates.T, size=5, symbol="o", pen=None, brush=(255, 0, 0)
         )
         self._unknown_obstacles_item = pg.ScatterPlotItem(
-            *self._unknown_obstacle_coordinates.T, size=5, symbol="o", pen=None, brush=(0, 255, 255)
+            *self._map_server_node.unknown_obstacle_coordinates.T, size=5, symbol="o", pen=None, brush=(0, 255, 255)
         )
         self._measured_state_item = CarItem(self._measured_state, color="w", with_lidar=True)
         self._pressed_pose_item = CarItem(self._measured_state, color="g")
@@ -152,8 +115,8 @@ class MainWindow(QMainWindow):
         self._global_planner_segments_items: list[pg.PlotCurveItem] = []
         self._trajectory_item = pg.PlotCurveItem(pen=pg.mkPen("c"))
         self._trajectory_item.setVisible(False)
-        self._plot_widget.addItem(self._known_obstacles_item)
         self._plot_widget.addItem(self._unknown_obstacles_item)
+        self._plot_widget.addItem(self._known_obstacles_item)
         self._plot_widget.addItem(self._trajectory_item)
         self._plot_widget.addItem(self._measured_state_item)
         self._plot_widget.addItem(self._goal_pose_item)
@@ -185,6 +148,7 @@ class MainWindow(QMainWindow):
 
         # connect signals
         self._car_simulation_node.measured_state.connect(self._local_planner_node.set_state)
+        self._car_simulation_node.measured_state.connect(self._map_server_node.update)
         self._car_simulation_node.measured_state.connect(self._update_measured_state)
         self._global_planner_node.display_segments.connect(self._update_global_planner_display_segments)
         self._global_planner_node.finished.connect(self._car_simulation_node.resume)
@@ -193,6 +157,7 @@ class MainWindow(QMainWindow):
         self._local_planner_node.control_sequence.connect(self._car_simulation_node.set_control_sequence)
         self._local_planner_node.local_trajectory.connect(self._update_local_trajectory)
         self._local_planner_node.reference_points.connect(self._update_reference_points)
+        self._map_server_node.known_obstacle_coordinates_updated.connect(self._update_known_obstacle_coordinates)
         self.canceled.connect(self._car_simulation_node.stop)
         self.canceled.connect(self._global_planner_node.cancel)
         self.canceled.connect(self._local_planner_node.cancel)
@@ -244,7 +209,7 @@ class MainWindow(QMainWindow):
             self.set_state.emit(state)
             self._goal_pose_item.setVisible(False)
         elif self._ui.set_goal_button.isChecked():
-            self.set_goal.emit(self._measured_state, state, Obstacles(self._known_obstacle_coordinates))
+            self.set_goal.emit(self._measured_state, state, Obstacles(self._map_server_node.known_obstacle_coordinates))
             self._goal_pose_item.set_state(state)
             self._goal_pose_item.setVisible(True)
             self._goal_unreachable_item.setPos(start_x, start_y)
@@ -276,6 +241,10 @@ class MainWindow(QMainWindow):
             self._car_simulation_stopped = False
         else:
             self._goal_unreachable_item.setVisible(True)
+
+    @Slot(np.ndarray)
+    def _update_known_obstacle_coordinates(self, known_obstacle_coordinates: npt.NDArray[np.floating[Any]]) -> None:
+        self._known_obstacles_item.setData(*known_obstacle_coordinates.T)
 
     @Slot(float, Car)
     def _update_measured_state(self, timestamp_s: float, state: Car) -> None:
